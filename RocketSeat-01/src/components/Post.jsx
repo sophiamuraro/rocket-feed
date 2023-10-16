925c5ce047142fd7197feb173a968a02b487b0b4
Sophia <sophiamperez@hotmail.com>
2023-10-16 15:16:56 -0300

    add profile and timeline

--- RocketSeat-01/src/components/Post.jsx
+++ RocketSeat-01/src/components/Post.jsx
@@ -1,18 +1,39 @@
-import styles from "./Post.module.css"
+import styles from "./Post.module.css";
 
 export function Post() {
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/souph1s.png" alt="" />
-                    <div className={styles.authorInfo}>
-                        <strong>Sophia Muraro</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
-                <time dateTime="2022-05-11 08:13:00">Publicado ha 1h</time>
-            </header>
-        </article>
-    )
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/souph1s.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Sophia Muraro</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:00">
+          Publicado ha 1h
+        </time>
+
+        <div className={styles.content}>
+          <p>Fala galeraa 👋</p>
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+          <p>
+            <a href="">👉 jane.design/doctorcare</a>
+          </p>
+          <p>
+            <a href="">#novoprojeto #nlw #rocketseat</a>
+          </p>
+        </div>
+      </header>
+    </article>
+  );
 }
