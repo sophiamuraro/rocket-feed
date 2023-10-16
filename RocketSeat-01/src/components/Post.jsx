319c52d2d0c7d9a60fc335b462410a7f20784ce2
Sophia <sophiamperez@hotmail.com>
2023-10-16 16:49:20 -0300

    styling post

--- RocketSeat-01/src/components/Post.jsx
+++ RocketSeat-01/src/components/Post.jsx
@@ -18,22 +18,24 @@ export function Post() {
         <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:00">
           Publicado ha 1h
         </time>
-
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-          <p>
-            <a href="">👉 jane.design/doctorcare</a>
-          </p>
-          <p>
-            <a href="">#novoprojeto #nlw #rocketseat</a>
-          </p>
-        </div>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href=""> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>{" "}
+        </p>
+      </div>
+
+     
     </article>
   );
 }
