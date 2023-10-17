c6f6abc0e83dfc5a498187bf8e1770c23163ff5a
Sophia <sophiamperez@hotmail.com>
2023-10-16 23:23:47 -0300

    creating comments field

--- RocketSeat-01/src/App.jsx
+++ RocketSeat-01/src/App.jsx
@@ -1,13 +1,11 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
-import { Post } from './components/Post'
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
-
-import './global.css'
-import styles from "./App.module.css"
+import "./global.css";
+import styles from "./App.module.css";
 
 function App() {
-
   return (
     <>
       <Header />
@@ -15,10 +13,11 @@ function App() {
         <Sidebar />
         <main>
           <Post />
+          <Post />
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
