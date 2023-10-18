6474aafb01699a56a816d2c5d36c5679f963a7d8
Sophia <sophiamperez@hotmail.com>
2023-10-17 22:36:23 -0300

    avatar component

--- RocketSeat-01/src/components/Comment.jsx
+++ RocketSeat-01/src/components/Comment.jsx
@@ -1,21 +1,22 @@
 import { ThumbsUp, Trash } from "phosphor-react";
-import style from "./Comment.module.css";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/souph1s.png" />
+      <Avatar hasBorder={false} src="https://github.com/souph1s.png" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Sophia Muraro</strong>
               <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:00">
                 Cerca de 1h atrás
               </time>
             </div>
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!!</p>
--- RocketSeat-01/src/components/Post.jsx
+++ RocketSeat-01/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/souph1s.png"
-            alt=""
-          />
+          <Avatar src="https://github.com/souph1s.png" />
           <div className={styles.authorInfo}>
             <strong>Sophia Muraro</strong>
             <span>Web Developer</span>
