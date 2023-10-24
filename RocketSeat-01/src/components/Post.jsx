1bc97ee197b46e20618d16dad067bb5cba2e5960
Sophia Muraro <sophia.l.muraroperez@exxonmobil.com>
2023-10-24 17:03:14 -0300

    manipulating dates

--- RocketSeat-01/src/components/Post.jsx
+++ RocketSeat-01/src/components/Post.jsx
@@ -2,23 +2,30 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post(props) {
+export function Post({ author, publishedAt }) {
+  const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(publishedAt)
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/souph1s.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Sophia Muraro</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
         <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:00">
-          Publicado ha 1h
+          {publishedDateFormatted}
         </time>
       </header>
       <div className={styles.content}>
-       
+
       </div>
 
       <form className={styles.commentForm}>
