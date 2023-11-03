d7a84b09951e46d09d194aa466ffb39a3b781b47
sophiamuraro <sophia.l.muraroperez@exxonmobil.com>
2023-11-03 09:33:13 -0300

    liking the comments

--- RocketSeat-01/src/components/Comment.jsx
+++ RocketSeat-01/src/components/Comment.jsx
@@ -1,11 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
-import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
+import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
-    onDeleteComment(content)
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment(){
+    setLikeCount(likeCount + 1);
   }
 
 
@@ -28,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
