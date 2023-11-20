6450ae9401e0ab23d4a288fb76eb397d24ff215d
sophiamuraro <sophia.l.muraroperez@exxonmobil.com>
2023-11-20 08:38:34 -0300

    interface extends typescript

--- rocket-feed-ts/src/App.tsx
+++ rocket-feed-ts/src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 
 import "./global.css";
 import styles from "./App.module.css";
 
-const posts = [
+
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -46,9 +47,7 @@ function App() {
           {posts.map(post => {
             return (<Post
               key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
+              post={post}
             />)
           })}
         </main>
--- rocket-feed-ts/src/components/Avatar.tsx
+++ rocket-feed-ts/src/components/Avatar.tsx
@@ -1,17 +1,15 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
-interface Avatar {
+interface Avatar extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: Avatar) {
+export function Avatar({ hasBorder = true, ...props}: Avatar) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
